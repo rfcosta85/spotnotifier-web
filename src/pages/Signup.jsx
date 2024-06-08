888989c1de9dfed78143e889d925ed5341b1f73d
rodrigocosta <rfcosta85@gmail.com>
2024-06-08 19:39:07 +0100

    Ajusta o signup

--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -70,19 +70,97 @@ function Signup() {
       
   }
   return (
-    <div className="App">
-      <form onSubmit={handleSubmit}>
-        <input type="text" placeholder='Insira o seu nome' name='name' onChange={handleChange} />
-        <input type="email" placeholder='Insira o seu email' name='email' onChange={handleChange} />
-        <input type="password" placeholder='Insira a sua palavra-passe' name='password' onChange={handleChange} />
-        <button type='submit'>
-          Registrar
-        </button>
-      </form>
-      Já possui uma conta? <Link to='/'>Login</Link>
+    <div className="flex flex-col items-center min-h-screen bg-gray-100">
+      <div className="w-full max-w-md p- ssl:p-6 bg-white shadow-md rounded-lg">
+        <div className="flex justify-center mb-4">
+          <img src="./assets/spotnotifier_logo.png" alt="Logo Spotnotifier" className="h-52 w-auto" />
+        </div>
+        <div className="text-center mb-16">
+          <h1 className="text-xl font-normal font-sans text-blue-light">Bem-vindo ao Spotnotifier</h1>
+        </div>
+  
+        <div className="mb-4">
+          <form onSubmit={handleSubmit} className="space-y-12">
+            <div>
+            <label htmlFor="" className='w-full px-1 py-2 text-blue-dark flex flex-col items-start'>
+               Primeiro Nome
+              </label>
+              <input
+                type="text"
+                placeholder='Insira o primeiro nome'
+                name='email'
+                onChange={handleChange}
+                className="w-full px-3 py-2 border font-sans border-blue-dark rounded-xl  focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
 
+              <label htmlFor="" className='w-full px-1 py-2 text-blue-dark flex flex-col items-start'>
+                Último Nome
+              </label>
+              <input
+                type="text"
+                placeholder='Insira o último nome'
+                name='email'
+                onChange={handleChange}
+                className="w-full px-3 py-2 border font-sans border-blue-dark rounded-xl  focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+              <label htmlFor="" className='w-full px-1 py-2 text-blue-dark flex flex-col items-start'>
+               Email
+              </label>
+              <input
+                type="email"
+                placeholder='Insira o seu Email'
+                name='email'
+                onChange={handleChange}
+                className="w-full px-3 py-2 border font-sans border-blue-dark rounded-xl  focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+              <div className='py-4'>
+              <label htmlFor="" className='w-full px-1 py-2  text-blue-dark flex flex-col items-start'>
+                Palavra-passe
+              </label>
+              <input
+                type="password"
+                placeholder='Insira a sua palavra-passe'
+                name='password'
+                onChange={handleChange}
+                className="w-full px-3 py-2 border font-sans border-blue-dark rounded-xl focus:outline-none focus:ring-2 focus:ring-indigo-500"
+              />
+              </div>
+            </div>
+            <button
+              type='submit'
+              className="w-full py-2 rounded-xl bg-primary font-sans text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+            >
+              Registar
+            </button>
+          </form>
+        </div>
+  
+        <div className="text-center mt-20">
+          <a href="/recovery-password" className="text-sm font-sans text-blue-light hover:underline">Esqueceu a sua palavra-passe</a>
+        </div>
+  
+        <div className="text-center mt-4 mb-16">
+          <p className="text-sm text-orange">
+            Já possui uma conta? <Link to='/' className="text-orange font-sans hover:underline">Login</Link>
+          </p>
+        </div>
+      </div>
     </div>
   );
+  // return (
+  //   <div className="App">
+  //     <form onSubmit={handleSubmit}>
+  //       <input type="text" placeholder='Insira o seu nome' name='name' onChange={handleChange} />
+  //       <input type="email" placeholder='Insira o seu email' name='email' onChange={handleChange} />
+  //       <input type="password" placeholder='Insira a sua palavra-passe' name='password' onChange={handleChange} />
+  //       <button type='submit'>
+  //         Registrar
+  //       </button>
+  //     </form>
+     
+
+  //   </div>
+  // );
 }
 
 export default Signup;
