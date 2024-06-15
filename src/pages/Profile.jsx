a840bee37d183d78e6d913501d41f9622d6579d3
rodrigocosta <rfcosta85@gmail.com>
2024-06-15 11:30:01 +0100

    Retira o icone x

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -5,21 +5,12 @@ function Profile () {
     return(
         <div className="flex flex-col items-center min-h-screen bg-gray-lightest">
             <div className="flex flex-col items-center h-full">
-            <div className="flex gap-6 mt-16">
-                <div className="flex justify-center mb-4">
-                    <img src="./assets/spotnotifier_logo.png" 
-                    alt="Logo Spotnotifier" className="h-52 w-auto" />
+                <div className="flex gap-6 mt-16">
+                    <div className="flex justify-center mb-4">
+                        <img src="./assets/spotnotifier_logo.png" 
+                        alt="Logo Spotnotifier" className="h-52 w-auto" />
+                    </div>                   
                 </div>
-
-                <div className="mt-4">
-                <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" 
-                    fill="#E25E3E" className="size-6">
-                    <path fillRule="evenodd" 
-                        d="M5.47 5.47a.75.75 0 0 1 1.06 0L12 10.94l5.47-5.47a.75.75 0 1 1 1.06 1.06L13.06 12l5.47 5.47a.75.75 0 1 1-1.06 1.06L12 13.06l-5.47 5.47a.75.75 0 0 1-1.06-1.06L10.94 12 5.47 6.53a.75.75 0 0 1 0-1.06Z" 
-                    clipRule="evenodd" />
-                </svg>
-                </div>
-            </div>
 
             <div className="flex gap-4 items-center w-80">
                 <img src="./assets/human-icon.png"/>
