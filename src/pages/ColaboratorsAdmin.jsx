04cdc35a3de318a4fffef852f116e77c5ea8ee39
rodrigocosta <rfcosta85@gmail.com>
2024-06-16 22:28:27 +0100

    Ajustes página de edição de usuário

--- src/pages/ColaboratorsAdmin.jsx
+++ src/pages/ColaboratorsAdmin.jsx
@@ -64,8 +64,16 @@ function ColaboratorsAdmin() {
   }
 
   return (
-    <div className="container mx-auto px-4 py-8">
+    <div className="container mx-auto px-4 py-8">        
       <div className="flex flex-col items-center h-full">
+      <div className="w-96 mt-6 h-6">
+          <a href='/homepage'>
+              <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" strokeWidth={1.5} 
+                  stroke="#E25E3E" className="size-6">
+                  <path strokeLinecap="round" strokeLinejoin="round" d="M10.5 19.5 3 12m0 0 7.5-7.5M3 12h18" />
+              </svg>
+          </a>
+        </div>
         <div className="flex gap-6 mt-0">
           <div className="flex justify-center mb-4">
             <a href="/homepage">
@@ -78,7 +86,7 @@ function ColaboratorsAdmin() {
       <form onSubmit={handleSubmit} className="space-y-6 flex flex-col items-center">
         <div className="flex flex-col w-96 min-h-screen bg-gray-lightest gap-4">
           <div>
-            <label htmlFor="name" className="block text-sm font-medium text-blue-dark">
+            <label htmlFor="name" className="block text-sm font-medium text-blue-dark w-14">
               Nome
             </label>
             <input
@@ -92,7 +100,7 @@ function ColaboratorsAdmin() {
             />
           </div>
           <div>
-            <label htmlFor="last_name" className="block text-sm font-medium text-blue-dark">
+            <label htmlFor="last_name" className="block text-sm font-medium text-blue-dark w-16">
               Apelido
             </label>
             <input
@@ -106,7 +114,7 @@ function ColaboratorsAdmin() {
             />
           </div>
           <div>
-            <label htmlFor="email" className="block text-sm font-medium text-blue-dark">
+            <label htmlFor="email" className="block text-sm font-medium text-blue-dark w-14">
               Email
             </label>
             <input
@@ -120,7 +128,7 @@ function ColaboratorsAdmin() {
             />
           </div>
           <div>
-            <label htmlFor="role" className="block text-sm font-medium text-blue-dark">
+            <label htmlFor="role" className="block text-sm font-medium text-blue-dark w-12">
               Role
             </label>
             <input
@@ -134,7 +142,7 @@ function ColaboratorsAdmin() {
             />
           </div>
           <div>
-            <label htmlFor="hash_password" className="block text-sm font-medium text-blue-dark">
+            <label htmlFor="hash_password" className="block text-sm font-medium text-blue-dark w-28">
                 Palavra-passe
             </label>
             <input
